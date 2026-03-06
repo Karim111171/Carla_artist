70a0453fc8fc6360d0c7b796696d44cc1995dad5
Karim111171 <karim.keirouz@gmail.com>
2026-03-06 22:45:53 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll(".gallery-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const close = document.getElementById("close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+close.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
